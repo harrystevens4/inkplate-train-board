55612bcc663a553d2071ed01fee5aec6ee8df8c9
harry <harrywstevens4@gmail.com>
2026-03-16 19:34:58 +0000

    added tfl api support and changed display to cycle on touch

--- inkplate.ino
+++ inkplate.ino
@@ -31,32 +31,44 @@ void loop() {
 	static int selected_menu = 0;
 	switch (selected_menu){
 	case 0:
-		display_national_rail_departures("CFB","Cftd Brdg");
+		display_national_rail_departures("CFB","Caftord Bridge Dept.");
 		break;
 	case 1:
-		display_national_rail_departures("CTF","Catford");
+		display_national_rail_departures("CTF","Catford Dept.");
 		break;
 	case 2:
-		display_national_rail_departures("HPA","Hnr Oak Pk");
+		display_national_rail_departures("HPA","Hnr Oak Park Dept.");
+		break;
+	case 3:
+		//ravensbourne park crescent and bourneville road (same stop in opposite directions)
+		const char *stops[] = {"490011434Z","490000365Z"};
+		size_t stop_count = sizeof(stops)/sizeof(const char *);
+		display_tfl_arrivals(stops,stop_count,"284 Arrivals");
 		break;
 	}
-	//====== update screen ======
-	inkplate.display();
 	//====== wait for user input ======
 	const time_t time_between_updates_ms = 60000;
 	time_t time_waited_ms = 0;
 	while (time_waited_ms < time_between_updates_ms){
-		delay(100);
+		delay(50);
 		time_waited_ms += 100;
 		uint16_t x[2], y[2];
 		if (inkplate.tsGetData(x,y) > 0){
-			selected_menu = (selected_menu+1) % 3;
+			selected_menu = (selected_menu+1) % 4;
 			break;
 		}
 	}
 }
 
 int display_national_rail_departures(const char *crs_code, const char *title){
+	//====== display header ======
+	inkplate.clearDisplay();
+	inkplate.setTextColor(BLACK);
+	inkplate.setCursor(50,50);
+	inkplate.setTextSize(4);
+	inkplate.print(String(title));
+	inkplate.fillRect(50,90,500,10,BLACK);
+	inkplate.display();
 	//====== fetch train times ======
 	Serial.println("requesting departures from "+String(title)+"...");
 	HTTPClient http;
@@ -70,13 +82,6 @@ int display_national_rail_departures(const char *crs_code, const char *title){
 			//Serial.println(response);
 			JsonDocument json_response;
 			deserializeJson(json_response, response);
-			//====== display header ======
-			inkplate.clearDisplay();
-			inkplate.setTextColor(BLACK);
-			inkplate.setCursor(50,50);
-			inkplate.setTextSize(4);
-			inkplate.print(String(title)+" Departures");
-			inkplate.fillRect(50,90,500,10,BLACK);
 			//====== display each train time ======
 			inkplate.setTextSize(3);
 			int i = 0;
@@ -100,6 +105,8 @@ int display_national_rail_departures(const char *crs_code, const char *title){
 				inkplate.print(stated_time_of_departure);
 				i++;
 			}
+			//====== update screen ======
+			inkplate.display();
 			//====== display ======
 			Serial.println("departures displayed");
 		}
@@ -148,6 +155,60 @@ int display_selection_menu(const char **options, size_t option_count){
 			delay(100);
 		}
 	}
+}
+
+int display_tfl_arrivals(const char **stop_ids, size_t stop_count, const char *title){
+	//====== display header ======
+	inkplate.clearDisplay();
+	inkplate.setTextColor(BLACK);
+	inkplate.setCursor(50,50);
+	inkplate.setTextSize(4);
+	inkplate.print(String(title));
+	inkplate.fillRect(50,90,500,10,BLACK);
+	inkplate.display();
+	//====== for each stop point fetch the stops ======
+	HTTPClient http;
+	int display_line = 0;
+	for (int stop = 0; stop < stop_count; stop++){
+		const char *stop_id = stop_ids[stop];
+		//====== fetch from api ======
+		Serial.println("requesting tfl arrivals from "+String(stop_id)+"...");
+		if (http.begin("https://api.tfl.gov.uk/StopPoint/"+String(stop_id)+"/Arrivals")){
+			int http_response_code = http.GET();
+			if (http_response_code == 200){
+				//====== deserialise =====
+				String response = http.getString();
+				//Serial.println(response);
+				JsonDocument arrivals;
+				deserializeJson(arrivals, response);
+				//====== display each train time ======
+				inkplate.setTextSize(3);
+				if (arrivals.isNull()){
+					Serial.println("no services provided");
+					continue;
+				}
+				Serial.println("arrivals fetched successfully");
+				for (JsonVariant arrival : arrivals.as<JsonArray>()){
+					struct tm expected_arrival_tm = {0};
+					strptime(arrival["expectedArrival"] | "","%FT%TZ",& expected_arrival_tm);
+					char expected_arrival[25] = "";
+					strftime(expected_arrival,sizeof(expected_arrival)/sizeof(char),"%H:%M",&expected_arrival_tm);
+					String destination = arrival["destinationName"];
+					inkplate.setCursor(40,130+(35*display_line));
+					inkplate.print(destination);
+					inkplate.setCursor(475,130+(35*display_line));
+					inkplate.print(expected_arrival);
+					display_line++;
+				}
+				Serial.println("departures displayed");
+			}
+		}else {
+			Serial.println("could not connect");
+		}
+	}
+	//====== update screen ======
+	inkplate.display();
+	return 0;
 }
 
 int wifi_connect(){
